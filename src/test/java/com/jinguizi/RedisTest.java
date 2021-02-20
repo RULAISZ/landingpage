e86e289ab6af175a6be155ace3cf1cf66ab3c35f
RULAISZ <lzw1508730848@126.com>
2021-02-20 15:34:07 +0800

    移植项目

--- src/test/java/com/jinguizi/RedisTest.java
+++ src/test/java/com/jinguizi/RedisTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,5 +29,13 @@ public class RedisTest {
         Object ss = redisTemplate.opsForList().rightPop("ss");
         List ss1 = redisTemplate.opsForList().range("ss", 1, -1);
         System.out.println("ss1 = " + ss1);
+    }
+
+    @Test
+    public void test02(){
+        Date date = new Date(1613302877235L);
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SS");
+        String format = f.format(date);
+        System.out.println(format);
     }
 }
